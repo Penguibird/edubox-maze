bed2195fd52856ed269deb24977d6fe8c08e1a11
loskot <loskotaklp@gmail.com>
2025-01-14 17:19:14 +0100

    build

--- src/utils/debounce.tsx
+++ src/utils/debounce.tsx
@@ -1,11 +1,10 @@
+// @ts-nocheck
 export function debounce<T extends any[], U extends unknown>(func: (...args: T) => U, timeout = 300) {
   let timer: number;
   return (...args: T): Promise<U> => {
     clearTimeout(timer);
     return new Promise((res) => {
-      // @ts-expect-error
       timer = setTimeout(() => {
-        // @ts-expect-error
         res(func.apply(this, args));
       }, timeout);
     });
--- src/utils/sortValues.tsx
+++
@@ -1,3 +0,0 @@
-import numbify from './numbify';
-
-export const sortValues = ([keyA]: [string, any], [keyB]: [string, any]): number => numbify(keyA) - numbify(keyB);
